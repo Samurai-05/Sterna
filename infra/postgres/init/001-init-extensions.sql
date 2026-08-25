1104a8765685587e382cb938c5b9dd4c8b273c86
Romain <durusselromain@gmail.com>
2026-08-25 15:18:06 +0200

    reorganization of database files

--- infra/postgres/init/001-init-extensions.sql
+++
@@ -1,4 +0,0 @@
--- Enables spatial data types and functions needed for
--- storing coordinates and performing map-related queries
--- (country detection, distance to points of interest, etc.)
-CREATE EXTENSION IF NOT EXISTS postgis;
